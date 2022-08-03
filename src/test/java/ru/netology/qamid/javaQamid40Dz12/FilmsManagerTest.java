5692942beb5f20ddd17a10f567bc7596011d0eca
ChukharevIlya <chukharevowen@yandex.ru>
2022-08-03 18:53:36 +0300

    new tests added

--- src/test/java/ru/netology/qamid/javaQamid40Dz12/FilmsManagerTest.java
+++ src/test/java/ru/netology/qamid/javaQamid40Dz12/FilmsManagerTest.java
@@ -35,7 +35,7 @@ public class FilmsManagerTest {
     }
 
     @Test
-    public void shouldShowLastFilms() {
+    public void shouldShowLastFilmsIfAboveLimit() {
         FilmsManager manager = new FilmsManager();
 
         manager.addFilm("film1");
@@ -52,6 +52,45 @@ public class FilmsManagerTest {
         manager.addFilm("film12");
 
         String[] expected = {"film12", "film11", "film10", "film9", "film8", "film7", "film6", "film5", "film4", "film3"};
+        String[] actual = manager.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldShowLastFilmsIfUnderLimit() {
+        FilmsManager manager = new FilmsManager();
+
+        manager.addFilm("film1");
+        manager.addFilm("film2");
+        manager.addFilm("film3");
+        manager.addFilm("film4");
+
+
+        String[] expected = {"film4", "film3", "film2", "film1"};
+        String[] actual = manager.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldShowLastFilmsIfEqualsToLimit() {
+        FilmsManager manager = new FilmsManager(12);
+
+        manager.addFilm("film1");
+        manager.addFilm("film2");
+        manager.addFilm("film3");
+        manager.addFilm("film4");
+        manager.addFilm("film5");
+        manager.addFilm("film6");
+        manager.addFilm("film7");
+        manager.addFilm("film8");
+        manager.addFilm("film9");
+        manager.addFilm("film10");
+        manager.addFilm("film11");
+        manager.addFilm("film12");
+
+        String[] expected = {"film12", "film11", "film10", "film9", "film8", "film7", "film6", "film5", "film4", "film3", "film2", "film1"};
         String[] actual = manager.findLast();
 
         Assertions.assertArrayEquals(expected, actual);
